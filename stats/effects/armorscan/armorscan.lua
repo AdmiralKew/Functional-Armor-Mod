a32ecb17452bd6b72c96a285a8589c809eaf8f7b
AdmiralKew <bien4500@gmail.com>
2015-07-02 21:57:50 +0800

    Scanning script added, still WIP

---
+++ stats/effects/armorscan/armorscan.lua
@@ -0,0 +1,15 @@
+function init()
+	script.setUpdateDelta(15)
+end
+
+function update(dt)
+	scan = world.entityQuery(mcontroller.position(), entity.configParameter("scanRange"))
+	for i, entityId in pairs(scan) do
+		scanResult = entityId
+		if world.entityType(scanResult) == "monster" or world.farmableStage(scanResult) ~= nil or world.containerSize(scanResult) ~= nil then
+			local positions = world.entityPosition(scanResult)
+			world.spawnProjectile("invisibleprojectile-scan", {positions[1], positions[2] + 2.5}, entity.id(), {0, 0})
+		scanResult = nil
+		end
+	end
+end
